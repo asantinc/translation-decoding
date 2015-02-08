ad5166c66521b17cf1313e176d0ffd1d05e985f3
asantinc <angiedaniela@gmail.com>
2015-02-08 03:04:19 +0000

    Wrong performance, but functional decoder that swaps sentences. Need to figure out which additional loops need to happen to make it increase the overall likelihood.

--- decoder/decode.py
+++ decoder/decode.py
@@ -11,7 +11,7 @@ optparser.add_option("-t", "--translation-model", dest="tm", default="data/tm", 
 optparser.add_option("-l", "--language-model", dest="lm", default="data/lm", help="File containing ARPA-format language model (default=data/lm)")
 optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to decode (default=no limit)")
 optparser.add_option("-k", "--translations-per-phrase", dest="k", default=1, type="int", help="Limit on number of translations to consider per phrase (default=1)")
-optparser.add_option("-s", "--stack-size", dest="s", default=10, type="int", help="Maximum stack size (default=1)")
+optparser.add_option("-s", "--stack-size", dest="s", default=1000, type="int", help="Maximum stack size (default=1)")
 optparser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,  help="Verbose mode (default=off)")
 opts = optparser.parse_args()[0]
 
@@ -26,9 +26,7 @@ def printStacks(stacks):
       for s in stack.keys():     
         sentence = extract_english(stack[s])
         bits = stack[s].bitmap
-        print bits
-        print sentence
-        print '\n'
+        print ', '.join(str(x) for x in bits) +' == '+ sentence
       
 def updateBitmap(bitmap, startA, startB):
   bits = list(bitmap)
@@ -59,7 +57,6 @@ def createHypothesis(stacks, stack_index, sentence, tm, lm, bitmap, h):
       for word in phrase.english.split():		            
         (lm_state, word_logprob) = lm.score(lm_state, word)	
         logprob += word_logprob			
-      #TODO: does it know about f???
       logprob += lm.end(lm_state) if stack_index == len(f) else 0.0		
       new_hypothesis = h._replace(logprob=logprob, lm_state=lm_state, predecessor=h, phrase=phrase, bitmap=bitmap)
 
@@ -79,8 +76,7 @@ for word in set(sum(french,())):
 
 sys.stderr.write("Decoding %s...\n" % (opts.input,))
 
-for f in french:
-  pdb.set_trace()
+for i,f in enumerate(french):
   hypothesis = namedtuple("hypothesis", "logprob, lm_state, predecessor, phrase, bitmap")
   initial_bitmap = [0] * len(f)
   initial_hypothesis = hypothesis(0.0, lm.begin(), None, None, initial_bitmap)
@@ -88,36 +84,23 @@ for f in french:
   stacks[0][lm.begin()] = initial_hypothesis
 
   for startA, stack in enumerate(stacks[:-1]):
-    printStacks(stacks)
-    print 'STACK #'+str(startA)+' = '+str(len(stack))
     for h in sorted(stack.itervalues(),key=lambda h: -h.logprob)[:opts.s]:
-        #pdb.set_trace()
-        print 'Starting point:'+str(h.bitmap).strip('[]')
         if (getMissingRange(h.bitmap) < 0):
           for end in range(startA+2, len(f)+1):
             for startB in range(startA+1, end+1):
-              print 'start A:'+str(startA)
-              print 'end:'+str(end)
-              print 'start B:'+str(startB)
               partA = f[startA:startB]
               bitmap_a = updateBitmap(h.bitmap, startA, startB)
-              print 'A = '+ str(bitmap_a).strip('[]')
               createHypothesis(stacks, sum(bitmap_a), partA, tm, lm, bitmap_a, h)
 
               if startB < end:  #we're not at the last loop
                 partB = f[startB:end] 
                 bitmap_b = updateBitmap(h.bitmap, startB, end)
-                print 'B = '+ str(bitmap_b).strip('[]')
                 createHypothesis(stacks, sum(bitmap_b), partB, tm, lm, bitmap_b, h)
-              print '\n'
 
         else:               #there's a gap
-          #print 'Stack index:'+str(startA)      
-          #pdb.set_trace()
           missed_range = getMissingRange(h.bitmap)
           missed_phrase = f[missed_range[0]:missed_range[1]]
           bitmap_updated = updateBitmap(h.bitmap, missed_range[0],missed_range[1])
-          print 'X = '+ str(bitmap_updated).strip('[]')
 
           if missed_phrase in tm:				                   
             for phrase in tm[missed_phrase]:			               
@@ -132,7 +115,6 @@ for f in french:
                  stacks[sum(bitmap_updated)][lm_state] = new_hypothesis
 
   winner = max(stacks[-1].itervalues(), key=lambda h: h.logprob)
-
   print extract_english(winner)
 
   if opts.verbose:
